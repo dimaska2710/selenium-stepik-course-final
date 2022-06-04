7c6a3b402645a29d9532620405dcb7cb13988b08
Dmitriy.Borisov <Dmitriy.Borisov@nexign.com>
2022-06-05 02:01:54 +0300

    4.2.8-page tasks have been done.

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,12 @@
-from selenium.webdriver.common.by import By
-
 from pages.base_page import BasePage
+from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_presented(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_presented(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
