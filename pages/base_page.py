f4def807d8e4875aef815a2f49d72d9e44ca8582
Dmitriy.Borisov <Dmitriy.Borisov@nexign.com>
2022-06-05 17:13:26 +0300

    4.3.9-page tasks have been done.

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,8 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
+from pages.locators import BasePageLocators
+
 
 class BasePage:
 
@@ -52,3 +54,10 @@ class BasePage:
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_presented(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
